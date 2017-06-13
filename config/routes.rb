6e57820aa798fffafcfe14e5fe7591ccbcd243b4
Tom Yip <tomyip0010@gmail.com>
2017-06-13 22:35:04 +0800

    Initialize project

---
+++ config/routes.rb
@@ -0,0 +1,9 @@
+Rails.application.routes.draw do
+
+  devise_for :users
+  root 'dashboard#index'
+
+  resources :customers, only: [:index, :show]
+
+  get "angular_test", to: "angular_test#index"
+end
